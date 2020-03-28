d690814a0df6d67db85c1509c58771be4e0a0259
mateuszrozek <mateusz.s.rozek@gmail.com>
2020-03-28 15:35:42 +0100

    fetching

--- src/components/simpleTable.js
+++ src/components/simpleTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -33,37 +33,77 @@ function createHeaders(weeks) {
 }
 const headers = createHeaders(weeks)
 
-function createJanAreas(weeks) {
-    var result = ["Jan"];
-    for (var i = 0; i < weeks.length; i++) {
-        result.push(weeks[i][1]);
-    }
-    return result;
-}
-function createMateuszAreas(weeks) {
-    var result = ["Mateusz"];
-    for (var i = 0; i < weeks.length; i++) {
-        result.push(weeks[i][2]);
-    }
-    return result;
-}
-function createRobertAreas(weeks) {
-    var result = ["Robert"];
-    for (var i = 0; i < weeks.length; i++) {
-        result.push(weeks[i][3]);
-    }
-    return result;
-}
-const janAreas = createJanAreas(weeks);
-const mateuszAreas = createMateuszAreas(weeks);
-const robertAreas = createRobertAreas(weeks);
+// function createJanAreas(weeks) {
+//     var result = ["Jan"];
+//     for (var i = 0; i < weeks.length; i++) {
+//         result.push(weeks[i][1]);
+//     }
+//     return result;
+// }
+// function createMateuszAreas(weeks) {
+//     var result = ["Mateusz"];
+//     for (var i = 0; i < weeks.length; i++) {
+//         result.push(weeks[i][2]);
+//     }
+//     return result;
+// }
+// function createRobertAreas(weeks) {
+//     var result = ["Robert"];
+//     for (var i = 0; i < weeks.length; i++) {
+//         result.push(weeks[i][3]);
+//     }
+//     return result;
+// }
+// const janAreas = createJanAreas(weeks);
+// const mateuszAreas = createMateuszAreas(weeks);
+// const robertAreas = createRobertAreas(weeks);
 
 
 
 export default function SimpleTable() {
     const classes = useStyles();
+    const [hasError, setErrors] = useState(false);
+    const [janAreas, setJanAreas] = useState({});
+    const [robertAreas, setRobertAreas] = useState({});
+    const [mateuszAreas, setMateuszAreas] = useState({});
+
+
+    async function fetchJanData() {
+        const res = await fetch("localhost:8888/areas/1");
+        res
+            .json()
+            .then(res => setJanAreas(res))
+            .catch(err => setErrors(err));
+            console.log(res);
+    }
+
+    async function fetchMateuszData() {
+        const res = await fetch("localhost:8888/areas/2");
+        res
+            .json()
+            .then(res => setMateuszAreas(res))
+            .catch(err => setErrors(err));
+            console.log(res);
+    }
+
+    async function fetchRobertData() {
+        const res = await fetch("localhost:8888/areas/3");
+        res
+            .json()
+            .then(res => setRobertAreas(res))
+            .catch(err => setErrors(err));
+            console.log(res);
+    }
+
+    useEffect(() => {
+        fetchJanData();
+        fetchMateuszData();
+        fetchRobertData();
+    });
+
 
     return (
+        <div>
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
@@ -93,5 +133,9 @@ export default function SimpleTable() {
                 </TableBody>
             </Table>
         </TableContainer>
+    
+            <span>Has error: {JSON.stringify(hasError)}</span>
+        </div>
+
     );
 }
